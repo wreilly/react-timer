38ed291240dc3a2524b79648bd14350d86dfaf23
William Reilly <wreilly2001@gmail.com>
2016-08-19 15:45:07 -0400

    Automated Testing - Karma, Mocha

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,12 @@
+var expect = require('expect');
+
+// BEFORE the describe() function, this was just here as-is:
+// it('should properly run tests!', () => {
+//   expect(1).toBe(1);
+// });
+
+describe('App', () => {
+  it('should properly run tests!', () => {
+    expect(1).toBe(1);
+  });
+});
